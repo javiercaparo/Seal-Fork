90b53fbc7e49ca3fa608cd3a71ac40bd77136cbd
Javier Caparo <javier.caparo@gmail.com>
2025-01-30 14:21:18 -0500

    adding sonar scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.room) apply false
+    id("org.sonarqube") version "6.0.1.5171" // Replace with latest scanner version
 }
 
 buildscript {
@@ -19,3 +20,10 @@ tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
 
+sonar {
+  properties {
+    property("sonar.projectKey", "Seal-Fork")
+    property("sonar.organization", "javiercaparo") 
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
+}
